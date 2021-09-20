598ca6f2f6024b7622afedea403ed77b41c22e82
Touraj Khatibi <2rajpx@gmail.com>
2021-09-20 13:44:50 +0430

    install debug pack

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -11,4 +11,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
